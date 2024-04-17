c41b6f4c6d589a68c62b78eb3e144298fa68440a
eneskucukpehlivan1 <eneskucukpehlivan1@gmail.com>
2024-04-17 13:44:58 +0300

    Singleton için thread safe yapıldı

--- SingletonDesignPattern/Business/DatabaseBusiness.cs
+++ SingletonDesignPattern/Business/DatabaseBusiness.cs
@@ -9,13 +9,24 @@ namespace SingletonDesignPattern.Business
     public class DatabaseBusiness
     {
         private static DatabaseBusiness _databaseBusiness;
+        //Thread Safe için lock objesi eklendi
+        static object _lock = new object();
         private DatabaseBusiness() { }
         public static DatabaseBusiness GetInstance()
         {
-            if (_databaseBusiness == null)
+            //Thread Safe
+            lock (_lock)
             {
-                _databaseBusiness = new DatabaseBusiness();
+                if (_databaseBusiness == null)
+                {
+                    _databaseBusiness = new DatabaseBusiness();
+                }
             }
+            //Non Thread Safe
+            //if (_databaseBusiness == null)
+            //{
+            //    _databaseBusiness = new DatabaseBusiness();
+            //}
             return _databaseBusiness;
         }
     }
