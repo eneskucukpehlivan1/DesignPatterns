2d5661778d31b4cb56b81a13d5f5da3c0ad19c2b
eneskucukpehlivan1 <eneskucukpehlivan1@gmail.com>
2024-04-17 22:30:59 +0300

    Prototype Design Pattern ile ilgili örnek eklendi.

---
+++ PrototypeDesignPattern/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using PrototypeDesignPattern.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypeDesignPattern.Models
+{
+    public class Employee : PrototypeEmployee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public Employee(Guid Id, string Name, string Department)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Department = Department;
+        }
+
+        public override PrototypeEmployee? Clone()
+        {
+            return this.MemberwiseClone() as PrototypeEmployee;
+        }
+    }
+}
---
+++ PrototypeDesignPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using PrototypeDesignPattern.Models;
+
+//Prototype Desing Pattern ile birlikte constructor'ında çok fazla eleman isteyen bir sınıfıno anki kopyası alınıp o kopya üzerinden işlem yapılabilir.
+//Bu şekilde new'leme maliyetinden kurtulmuş olunur. Aynı zamanda her yeni nesne için constructor'daki tüm özellikleri tek tek girmeye gerek kalmaz.
+
+Employee employee = new Employee(Guid.NewGuid(), "Enes", "Yazılım");
+Employee employee2 = (Employee)employee.Clone();
+employee2.Name = "Ahmet";
+employee2.Id = Guid.NewGuid();
+
+Console.WriteLine($"1. çalışan isim: {employee.Name} | 2. çalışan isim: {employee2.Name}");
+Console.ReadLine();
