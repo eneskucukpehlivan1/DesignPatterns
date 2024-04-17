180812551959788bd422092e1f3e0f1f9c230a5b
eneskucukpehlivan1 <eneskucukpehlivan1@gmail.com>
2024-04-17 13:40:01 +0300

    Singleton Design Pattern ile ilgili örnek eklendi.

---
+++ SingletonDesignPattern/Business/DatabaseBusiness.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern.Business
+{
+    public class DatabaseBusiness
+    {
+        private static DatabaseBusiness _databaseBusiness;
+        private DatabaseBusiness() { }
+        public static DatabaseBusiness GetInstance()
+        {
+            if (_databaseBusiness == null)
+            {
+                _databaseBusiness = new DatabaseBusiness();
+            }
+            return _databaseBusiness;
+        }
+    }
+}
---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,4 @@
+﻿using SingletonDesignPattern.Business;
+
+//Singleton ile birlikte DB sınıfı gibi tek seferlik oluşması gereken sınıfları bir kez oluşturup daha sonrasında o insance'ı istediğimiz yerde çağırıp kullanabiliriz.
+DatabaseBusiness databaseBusiness = DatabaseBusiness.GetInstance();
